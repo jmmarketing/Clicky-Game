2dc491f9ca90ab451bcdec48fba99f1f6312a5bb
JeffM <jeff@jeffreymclean.com>
2019-10-27 16:23:16 -0700

    Started building out Game Logic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Header from "./components/Header"
 import Game from "./components/Game"
 import Wrapper from "./components/Wrapper"
 import GameCard from "./components/GameCard"
+import characters from "./characters.json"
 
-function App() {
-  return (
-    
-    <Wrapper>
-      <Header />
-        <Game/>
-          <GameCard />
+
+class App extends Component {
+
+
+
+  render() {
+    return (
+
+      <Wrapper>
+        <Header />
+        <Game />
+        <GameCard />
 
       </Wrapper>
-      
-  );
+
+    );
+  }
 }
 
 export default App;
