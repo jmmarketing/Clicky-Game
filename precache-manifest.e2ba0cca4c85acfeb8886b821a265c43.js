d02eb96a456d630fe278a46108715de2052705c0
JeffM <jeff@jeffreymclean.com>
2019-10-28 20:33:11 -0700

    Updates

---
+++ precache-manifest.e2ba0cca4c85acfeb8886b821a265c43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f011ce0b14443a19928ceda933b7d34",
+    "url": "/jmmarketing/Clicky-Game/index.html"
+  },
+  {
+    "revision": "4fd17a4dd0ba1acd2bbe",
+    "url": "/jmmarketing/Clicky-Game/static/css/main.d640b9e1.chunk.css"
+  },
+  {
+    "revision": "f189f686c3cfc58dc824",
+    "url": "/jmmarketing/Clicky-Game/static/js/2.bd3a9e65.chunk.js"
+  },
+  {
+    "revision": "4fd17a4dd0ba1acd2bbe",
+    "url": "/jmmarketing/Clicky-Game/static/js/main.aed63f8c.chunk.js"
+  },
+  {
+    "revision": "a4933557474b5728ffea",
+    "url": "/jmmarketing/Clicky-Game/static/js/runtime-main.f5c4805f.js"
+  }
+]);
