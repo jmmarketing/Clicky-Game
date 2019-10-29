7463dc9635e602da61564b4b1b6095ba21a39429
JeffM <jeff@jeffreymclean.com>
2019-10-28 20:15:39 -0700

    Add On click and End Game functions

--- src/App.js
+++ src/App.js
@@ -15,7 +15,54 @@ class App extends Component {
     highScore:0
   }
 
-clickCount = id => {}
+
+  componentDidMount(){
+    this.setState({characters: this.shuffle(this.state.characters)})
+  }; 
+
+
+  shuffle(array) {
+    // Code found here: https://stackoverflow.com/questions/39680997/shuffle-javascript-array
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
+
+clickCount = id => {
+  this.state.characters.find((object, index) => {
+    if (object.id === id && characters[index].clicked === false){
+      characters[index].clicked = true;
+      this.setState({score: this.state.score +1});
+      this.shuffle(characters);
+    } else{
+      this.endGame(); 
+    }
+
+  })
+
+}
+
+endGame(){
+  if (this.state.score > this.state.highScore){
+    this.setState({highScore: this.state.score});
+    this.state.characters.forEach(character => character.clicked = false);
+    this.setState({score: 0}); 
+  }
+}
 
   render() {
     return (
@@ -27,7 +74,7 @@ clickCount = id => {}
         <GameCard 
           clickCount={this.clickCount}
           id={character.id}
-          key={character.name}
+          key={character.id}
           image={character.image}
         />
         ))}
